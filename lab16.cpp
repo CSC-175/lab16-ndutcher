b64c5e5ec0b233e233111a29cc6aa5dcc1190858
ndutcher <ndutcher@umich.edu>
2025-03-31 14:08:46 -0400

    attempt 1

--- lab16.cpp
+++ lab16.cpp
@@ -1,1 +1,11 @@
 // Code to implement the gcf function goes here
+
+#include <iostream>
+using namespace std;
+
+int gcf(int a, int b) {
+  if (b == 0) {
+    return a;
+  }
+  return gcf(b, a % b);
+}
